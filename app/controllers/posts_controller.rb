944b9a26c74f4b7d8c6a607f7aea2e55f71bc896
Dmytro Stoliaruk <dmytro.stoliaruk@gmail.com>
2024-05-24 17:22:49 +0300

    delete json format

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,10 +26,8 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.save
         format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
   end
